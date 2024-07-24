d43ab1b2a21dd8eb122c5ccdf7fa81f614faf276
José Nieto <jnieto.drimo@bicevida.cl>
2024-07-24 19:59:52 -0400

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5fe35243b99649b582cbddb4e80bc5d2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a6ede5db-911c-4eff-bf9e-9f53ffc68c52'
+};
